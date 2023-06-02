cb5430a6d1f0613273699f55bfc2039aeaa8690b
Полина Рыжакова <polinochka_-95@mail.ru>
2023-06-02 21:58:35 +0300

    HW_3

---
+++ home_work3.sql
@@ -0,0 +1,53 @@
+USE home_work3;
+CREATE TABLE staff (
+	id INT AUTO_INCREMENT PRIMARY KEY, 
+	firstname VARCHAR(45),
+	lastname VARCHAR(45),
+	post VARCHAR(100),
+	seniority INT, 
+	salary INT, 
+	age INT
+);
+INSERT INTO staff (firstname, lastname, post, seniority, salary, age)
+VALUES
+('Вася', 'Петров', 'Начальник', '40', 100000, 60),
+('Петр', 'Власов', 'Начальник', '8', 70000, 30),
+('Катя', 'Катина', 'Инженер', '2', 70000, 25),
+('Саша', 'Сасин', 'Инженер', '12', 50000, 35),
+('Иван', 'Иванов', 'Рабочий', '40', 30000, 59),
+('Петр', 'Петров', 'Рабочий', '20', 25000, 40),
+('Сидр', 'Сидоров', 'Рабочий', '10', 20000, 35),
+('Антон', 'Антонов', 'Рабочий', '8', 19000, 28),
+('Юрий', 'Юрков', 'Рабочий', '5', 15000, 25),
+('Максим', 'Максимов', 'Рабочий', '2', 11000, 22),
+('Юрий', 'Галкин', 'Рабочий', '3', 12000, 24),
+('Людмила', 'Маркина', 'Уборщик', '10', 10000, 49);
+
+SELECT *
+FROM staff
+ORDER BY salary;
+
+SELECT *
+FROM staff
+ORDER BY salary DESC;
+
+SELECT salary
+FROM staff
+ORDER BY salary DESC
+LIMIT 5;
+
+SELECT post, SUM(salary) AS Total_salary
+FROM staff
+GROUP BY post;
+
+SELECT COUNT(id)
+FROM staff
+WHERE post="Рабочий" and age>=24 and age<=49;
+
+SELECT COUNT(DISTINCT post) AS count_post
+FROM staff;
+
+SELECT post, AVG(age) as avg_age
+FROM staff
+GROUP BY post
+HAVING AVG(age)<30
